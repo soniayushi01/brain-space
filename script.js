651859325451b966fea7114de55778894faf5a54
sohelhussain <sohelhussaing@gmail.com>
2023-12-14 19:11:59 -0800

    working on a image change effect on h1

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ display.forEach((dets)=>{
 })
 }
 viweImage();
+
+const viwe = () => {
+    document.querySelector(`.top-text`).addEventListener("click",(dets)=>{
+        // update the image and change the color of h1 while pressing the h1
+        if(dets.target.innerHTML === "Design"){ 
+            console.log(dets.target.innerHTML);
+            document.querySelector(`.top-line .innner-line`).style.transform = "translate(0%, 0%)";
+            document.querySelector(`.top-line .innner-line`).style.top = `0%`;
+            document.querySelector(`.top-text h1:nth-child(1)`).style.color = `white`;
+            console.log(flag);
+            // document.querySelector(`.right-cont`).style.backgroundImage = `URL(${})`;
+        }else if(dets.target.innerHTML === "Project"){
+            document.querySelector(`.top-text h1:nth-child(2)`).style.color = `white`;
+            document.querySelector(`.top-line .innner-line`).style.top = `25%`;
+            document.querySelector(`.top-line .innner-line`).style.transform = "translate(0%, 25%)";
+            flag = 2
+            // document.querySelector(`.right-cont`).style.backgroundImage = `URL(${https://uploads-ssl.webflow.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15d0_Project.webp})`;
+            console.log(dets.target.innerHTML);
+        }else if(dets.target.innerHTML === "Execution"){
+            document.querySelector(`.top-text h1:nth-child(3)`).style.color = `white`;
+            document.querySelector(`.top-line .innner-line`).style.top = `50%`;
+            document.querySelector(`.top-line .innner-line`).style.transform = "translate(0%, 50%)";
+            console.log(dets.target.innerHTML);
+        }
+    })
+}
+viwe();
